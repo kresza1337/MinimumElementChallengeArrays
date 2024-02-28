dab5566416088f27d9002390e1ca4941e3e520a6
Kresza1337 <krechaxdkrechaxd@gmail.com>
2024-02-28 19:07:06 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int array[] = readIntegers();
         System.out.println(findMin(array));
-
-
+        //test
     }
 
     public static int[] readIntegers(){
